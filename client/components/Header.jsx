e37b274e34594783acff824d2760dc84389f7828
Alvaro Hudson <the_friendly_giant22@Alvis-Air.local>
2022-07-15 10:12:23 +1200

    remove boilerplate data from actions and apiClient. Update app and header component

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Learn from './Learn'
+// import Path from './Path'  For stretch
 
 function Header() {
   return (
-    <div>Header</div>
+   <header className='header'>
+      <h1>MAP LEARN</h1>
+
+      <Link to={<Learn />}> <p>Learn</p> </Link>
+
+      <Link to={<Path />}> <p>Path</p> </Link>
+      
+   </header>
   )
 }
 
