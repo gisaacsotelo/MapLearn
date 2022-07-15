44cbe0dae17952c907e7d1e558f99e6b109e142f
Alvaro <98783170+alvaro-hudson@users.noreply.github.com>
2022-07-15 14:52:04 +1200

    CountryInfo Component

--- client/components/CountryInfo.jsx
+++ client/components/CountryInfo.jsx
@@ -1,17 +1,26 @@
 import React from "react"
 
 function CountryInfo(props) {
-  const { capital, languages, area, population } = props.country
+  const { flags, coatOfArms, name, capital, languages, area, population } =
+    props.country
 
   return (
-    <div>
-      <p>Capital: {capital}</p>
-      {/* {languages.map(language => {
-        return <p>{language}</p>
-      }) } */}
-      <p>{area}</p>
-      <p>{population}</p>
-    </div>
+    <>
+      <div className="countryName">
+        <h2>{name[Object.keys(name)[0]]}</h2>
+      </div>
+      <img className="countryFlag" src="https://flagcdn.com/w320/nz.png" />
+      <img
+        className="countryCoat"
+        src={coatOfArms[Object.keys(coatOfArms)[0]]}
+      />
+      <div className="countryDesc">
+        <p>Capital: {capital}</p>
+        <p>Primary Language: {languages[Object.keys(languages)[0]]}</p>
+        <p>Area: {area.toLocaleString()} km²</p>
+        <p>Population: {population.toLocaleString()} people</p>
+      </div>
+    </>
   )
 }
 
