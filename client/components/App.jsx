421fe4d549f4d450f3ac5e910858e83adafee009
Alvaro <98783170+alvaro-hudson@users.noreply.github.com>
2022-07-18 10:20:32 +1200

    header and footer change

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 // import view components
-import Learn from './Learn'
+import Learn from "./Learn"
 // import components
 
-import Header from './Header'
-import Footer from './Footer'
+import Header from "./Header"
+import Footer from "./Footer"
 
-function App () {
-
+function App() {
   return (
     <Router>
-
-      <div className="main">
       <Header />
+      <div className="main">
         <Routes>
-          <Route path='/' element={<Learn />}/>
-          <Route path='/play' element={<Learn />}/>
+          <Route path="/" element={<Learn />} />
+          <Route path="/play" element={<Learn />} />
 
           {/* <Route path='/play' element={<Play />} />   This will be for our stretch */}
         </Routes>
-      <Footer />
       </div>
-
+      <Footer />
     </Router>
   )
 }
