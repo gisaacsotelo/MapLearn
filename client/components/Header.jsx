4d09504da4063bab6ec7d720f695bcc5ff900355
Jore Toa Temarama <j.toatemarama@gmail.com>
2022-07-19 01:46:51 +1200

    added name changing functionality

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -12,7 +12,17 @@ function Header() {
   const [isOn, setIsOn] = useState(false);
 
   const toggleSwitch = () => setIsOn(!isOn);
+  
+  let status = () => "Learn"
+  const title = 'Map Learn!'
 
+  if (status === 'Learn'){
+    title = 'Map Learn!'
+    console.log(status)
+  } else if (status === 'Play'){
+    title = 'Map Play!'
+    console.log(status)
+  }
 
   const spring = {
     type: "spring",
@@ -31,7 +41,7 @@ function Header() {
         px="6"
         py="5"
         align="center"
-        justify="space-between"
+        justify="space-evenly"
       >
       {/* <motion.header className='heading' animate={{ x: 100 }} > */}
       <div className='earthgif'>
@@ -41,12 +51,12 @@ function Header() {
           whileHover={{ scale: 1.1 }}
           onHoverStart={e => {}}
           onHoverEnd={e => {}}>
-        <Heading size='lg' fontSize='50px' className="title">Map Learn! 	&#128506;</Heading>
+        <Heading size='lg' fontSize='50px' className="title">{title} 	&#128506;</Heading>
       {/* </motion.header> */}
       </motion.div>
       <nav>
-        <Link className='button-38' to={'/'}> Learn </Link>
-        <Link className='button-38' to={'/play'}> Play </Link>
+        <Link onClick={status='Learn'} className='button-38' to={'/'}> Learn </Link>
+        <Link onClick={status='Play'} className='button-38' to={'/play'}> Play </Link>
       </nav>
       </Flex>
     </chakra.header>
