0cf9bf0799d684349a9ec3862d22e6335f15a48b
Alvaro <98783170+alvaro-hudson@users.noreply.github.com>
2022-07-19 14:21:24 +1200

    Add footer text and basic css for it

--- todo.js
+++ todo.js
@@ -2,6 +2,8 @@
 TODO:
 
 - Avoid multiple selection while selecting right answer on Play
+- Sometimes the incorrect answer remains when it should disappear
+- Sometimes when you click on the correct country a new random country name appears before you can click next guess
 
 - Zoom in general - not working
 */
