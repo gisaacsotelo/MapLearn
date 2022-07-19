aa8c4a30e0ad0bab9b3c7f552c307495503d1740
isaac sotelo <gisaac.sotelo@gmail.com>
2022-07-19 14:26:37 +1200

    pulling changes

--- client/components/Play.jsx
+++ client/components/Play.jsx
@@ -6,47 +6,61 @@ import LeaderBoard from "./LeaderBoard"
 
 
 function Play() {
-  const [clicked, setClicked] = useState(null)
-  const [randomCountry, setRandomCountry] = useState(null)
-  const [reset, setReset] = useState(false)
+  const [clickedCountry, setClickedCountry] = useState(null) // contains country clicked by user
+  const [randomCountry, setRandomCountry] = useState(null) // contains random country for the round
+  const [answer, setAnswer] = useState('')  // variable that hold if the answer was right or wrong to print
+  const [turn, setTurn] = useState(0) // holds current turn
+  const [score,setScore] = useState(0) // holds score of game
+  const [reset, setReset] = useState(false) 
   const [showLeaderBoard, setyShowLeaderBoard] = useState(false)
   console.log(`Render component: `,randomCountry)
+  if (randomCountry) {console.log(`name: `,randomCountry.dataset.name)}
 
+  // VARIABLES
+
+  // colors:
+  const green = '#aeeb2bcf'
+  const red = 'rgb(216, 48, 82)'
+  const grey = 'rgba(255, 255, 255, 0.512)'
+
+
+  // on mounting & when reset is modified
   useEffect(() => {
     const countriesDOM = Array.from(document.querySelectorAll('path'))
-    console.log(`START ARRAY: `, countriesDOM)
+    console.log(`Effect - Mx: `, countriesDOM[93]) // prints mexico for reference 
     // console.log(`countriesDOM: `, countriesDOM)
-    const rndInt = Math.floor(Math.random() * `${countriesDOM.length}`) + 1
-    const rndmCntry = countriesDOM[rndInt].dataset
+    const rndInt = Math.floor(Math.random() * `${countriesDOM.length}`) //random number from 0 to array.length
+    const rndmCntry = countriesDOM[rndInt]
+    console.log(`mount-country: `, rndmCntry.dataset.name)
+    // console.log(`mount-id: `, rndmCntry.id)
+    // console.log(`mount-name: `, rndmCntry.dataset.name)
+    // console.log(`mount-name: `, rndmCntry.localName)
     setRandomCountry(rndmCntry)
   }, [reset])
 
-  //// create array of all countries 
-  //// select random country (#2) 
-  //// display selected country to find by user
-  // after clicked: 
-  // check clicked if matched w array selection -> point?
-  // if not: one chance used, restard selection #2
+  //  ^ FUNCTIONS
 
+
+  // ~ countryClicked
   //this function will run when a country is clicked in WorldMap
   const countryClicked = (e) => {
     const id = e.target.id
     const clickedCountry = Array.from(document.querySelectorAll(`path#${id}`))
-    const correctCountry = Array.from(document.querySelectorAll(`path#${randomCountry.id}`))
-
-    correctCountry[0].style.fill = '#aeeb2bcf'
-    setClicked(clickedCountry)
-    setReset(false)
+    console.log(`Country clicked: `,clickedCountry)
+    setClickedCountry(clickedCountry)
+    randomCountry.style.fill = green
     if (id === randomCountry.id){
-      console.log(`Right`)
-      
+      console.log(`Selectrion was RIGHT`)
+      setAnswer('CORRECT!')
     }else {
-      console.log(`You dumb fuck`)
-      clickedCountry[0].style.fill = 'rgb(216, 48, 82)'
+      console.log(`Selection was WRONG`)
+      setAnswer('WRONG!')
+      clickedCountry[0].style.fill = red
     }
-    // console.log(id)
+    setReset(false)
   }
-
+  
+  // ~nextGuess
   const nextGuess = e => {
     console.log(`CLICKED COUNTRY: `, clicked)
     const correctCountry = Array.from(document.querySelectorAll(`path#${randomCountry.id}`))
@@ -60,12 +74,21 @@ function Play() {
     console.log(`next!`)
   }
 
+  // ! RETURN
+  console.log(`ANSWER: `, answer)
   return (
     <>
-      {randomCountry && <h2 className="play-country-title">{randomCountry.name}</h2>}
+      {randomCountry && <h2 className="play-country-title">{randomCountry.dataset.name}</h2>}
+        <p className="turn">Turn: {turn}/10</p>
+        <p className="score">Score: {turn}pts</p>
       <WorldMap countryClicked={countryClicked} />
       {showLeaderBoard && <LeaderBoard />}
-      {clicked && <button className="btn-next" onClick={nextGuess}>Next Guess</button>}
+      {clickedCountry &&
+      <> 
+        <button className="btn-next" onClick={nextGuess}>Next Guess</button>
+        {<p className="answer">Your answer was: {answer}</p>}
+      </>
+      }
     </>
   )
 }
--- todo.js
+++ todo.js
@@ -2,6 +2,8 @@
 TODO:
 
 - Avoid multiple selection while selecting right answer on Play
+- Sometimes the incoreect answer remains when it shopuld dissapear.
+- hwen you get it right it changes name right away - fix that shait
 
 - Zoom in general - not working
 */
