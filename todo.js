d8928280811441e1af218ec397e49d6e57a4b2d1
isaac sotelo <gisaac.sotelo@gmail.com>
2022-07-20 10:15:20 +1200

    changing branch

--- todo.js
+++ todo.js
@@ -1,14 +1,17 @@
 /* 
 TODO:
 - limit to ten points
+- reset turn
+- reset game
 - add reset to scoreSummary
   -make scoreSummary appear/disappear 
   -CSS for scoreSummary
-- reset turn
-- reset game
+- remove countries every time they get selected and fix reset. 
+
+
 - Have music play on Play
 - green circle
-
+- play button stuff
 
 - Zoom in general - not working
 */
