0eb62f69394e9e492fd9b837285d8268cb338d0e
isaac sotelo <gisaac.sotelo@gmail.com>
2022-07-20 12:35:40 +1200

    luch

--- todo.js
+++ todo.js
@@ -6,12 +6,13 @@ TODO:
 //- reset game
 //- add reset to scoreSummary
   //-make scoreSummary appear/disappear 
-  -CSS for scoreSummary
+  - CSS for scoreSummary
 - remove countries every time they get selected and fix reset. 
 
 
 - Have music play on Play
 - green circle
+
 - play button stuff
 
 - Zoom in general - not working
